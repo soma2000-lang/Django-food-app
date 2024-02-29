8152bc5c9836a416f4f721df313f9f891b1208f4
Somasree Majumder <bishnupadamajumder32@gmail.com>
2024-03-01 01:45:33 +0530

    add

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserInfoForm, UserProfileForm
+from accounts.models import UserProfile
+from django.contrib import messages
+from orders.models import Order, OrderedFood
+import simplejson as json

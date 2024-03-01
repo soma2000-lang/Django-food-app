0ce21660ef148f87a73b7d4d42bbc6b7e2fe918a
Somasree Majumder <bishnupadamajumder32@gmail.com>
2024-03-01 22:44:47 +0530

    add

--- resturant/urls.py
+++ resturant/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for resturant project.
+"""foodOnline_main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from marketplace import views as MarketplaceViews
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
+    path('', views.home, name='home'),
+    path('', include('accounts.urls')),
 
-]
+    path('marketplace/', include('marketplace.urls')),
+
+    # CART
+    path('cart/', MarketplaceViews.cart, name='cart'),
+    # SEARCH
+    path('search/', MarketplaceViews.search, name='search'),
+
+    # CHECKOUT
+    path('checkout/', MarketplaceViews.checkout, name='checkout'),
+
+    # ORDERS
+    path('orders/', include('orders.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

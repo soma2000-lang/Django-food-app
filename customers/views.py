735ee545cd7819fb6c1e52f1a0ef000228d6df62
Somasree Majumder <bishnupadamajumder32@gmail.com>
2024-03-01 19:17:45 +0530

    add

--- customers/views.py
+++ customers/views.py
@@ -10,4 +10,43 @@ from orders.models import Order, OrderedFood
 import simplejson as json
 
 def cprofile(login_required=login):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        user_form = UserInfoForm(request.POST, instance=request.user)
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, 'Profile updated')
+            return redirect('cprofile')
+    else:
+            print(profile_form.errors)
+            print(user_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
+
+    context = {
+        'profile_form': profile_form,
+        'user_form' : user_form,
+        'profile': profile,
+    }
+    return  render(request,'customer/cprofile.html',context)
+
+def order_detail(request, order_number):
+    order_number=Order.objects.get(order_number=order_number,is_ordered=True)
+    orderedFood=Order.objects.filter(order=order)
+    int subtotal=0
+    for item in ordered_food:
+            subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+        context = {
+            'order': order,
+            'ordered_food': ordered_food,
+            'subtotal': subtotal,
+            'tax_data': tax_data,
+        }
+        return render(request, 'customers/order_detail.html', context)
+    except:
+        return redirect('customer')
     
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from urllib import response
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from marketplace.models import Cart, Tax
+from marketplace.context_processors import get_cart_amounts
+from menu.models import FoodItem
+from .forms import OrderForm
+from .models import Order, OrderedFood, Payment
+import simplejson as json
+from .utils import generate_order_number, order_total_by_vendor
+from accounts.utils import send_notification
+from django.contrib.auth.decorators import login_required
+import razorpay
+from resturant.settings import RZP_KEY_ID, RZP_KEY_SECRET
+from django.contrib.sites.shortcuts import get_current_site
 
-# Create your views here.
+client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
